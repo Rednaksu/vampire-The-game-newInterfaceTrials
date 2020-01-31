f5b601e925fb1eabcca63b2e31c9bbbe481e3077
rednaksu <lakhalskander@gmail.com>
2020-01-31 18:27:43 +0100

    Initial Commit

---
+++ newInterfaceTrials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newInterfaceTrials
+//
+//  Created by skander lakhal on 1/31/20.
+//  Copyright © 2020 REDRebelRiots. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
